16cd3ea2a7bd53c72514faa2bd4963cd380c1e78
cx259 <351538341@qq.com>
2015-11-04 21:06:44 +0800

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -16,39 +16,3 @@ plot(household200702$datetime,household200702$Global_active_power,xlab = "",ylab
 #axis(1,at=g_range2,lab=c("Thu","Fri","Sta"))
 #axis(2,at=2*0:g_range1[2])
 dev.off()
-###########################################################
-#plot3.png
-as.numeric(household200702$Sub_metering_1)->household200702$Sub_metering_1
-as.numeric(household200702$Sub_metering_2)->household200702$Sub_metering_2
-as.numeric(household200702$Sub_metering_3)->household200702$Sub_metering_3
-
-png(file="plot3.png")
-plot(household200702$datetime,household200702$Sub_metering_1,type="s",xlab="",ylab="Energy sub metering")
-lines(household200702$datetime,household200702$Sub_metering_2,type="s",col="red")
-lines(household200702$datetime,household200702$Sub_metering_3,type="s",col="blue")
-legend("topright",lty=c(1,1,1),col=c("black","red","blue"),legend=c("Sub_metering_1", 
-                                                                    "Sub_metering_2","Sub_metering_3"))
-dev.off()
-#########################################################
-as.numeric(household200702$Global_reactive_power)->household200702$Global_reactive_power
-as.numeric(household200702$Voltage)->household200702$Voltage
-#plot4.png
-png(file="plot4.png")
-par(mfrow=c(2,2))
-with(household200702,{
-  plot(household200702$datetime,household200702$Global_active_power,xlab = "",ylab="Global Active Power(kilowatts)",type="l")
-})
-
-with(household200702,{
-  plot(household200702$datetime,household200702$Voltage,xlab = "datetime",ylab="Voltage",type="l")
-})
-with(household200702,{
-  plot(household200702$datetime,household200702$Sub_metering_1,type="s",xlab="",ylab="Energy sub metering")
-  lines(household200702$datetime,household200702$Sub_metering_2,type="s",col="red")
-  lines(household200702$datetime,household200702$Sub_metering_3,type="s",col="blue")
-  legend("topright",lty=c(1,1,1),col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),bty = "n")
-})
-with(household200702,{
-  plot(household200702$datetime,household200702$Global_reactive_power,xlab ="datetime",ylab="Global_rective_ power",type="l")
-})
-dev.off()
